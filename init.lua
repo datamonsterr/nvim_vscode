9e2c50e89c55585f5f1bcdf7fff0093d8f8c2c08
datamonsterr <phamthanhdat17092004@gmail.com>
2024-02-20 18:33:27 +0700

    migrate nvim-data to new path

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+local old_stdpath = vim.fn.stdpath
+vim.fn.stdpath = function(value)
+    if value == "data" then
+        return vim.fn.expand("~/.local/share/nvim")
+    end
+    return old_stdpath(value)
+end
+
+vim.opt.packpath:remove(old_stdpath("data") .. "/site")
+vim.opt.packpath:append(vim.fn.expand("~/.local/share/nvim/site"))
+
 vim.opt.runtimepath:append("~/.config/nvim")
 require("plugins")
 require("mappings")
